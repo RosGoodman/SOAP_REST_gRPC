4c73ed9c2cfed6bebe8872f7e2715d5821913e61
Pavel <pavel.official@bk.ru>
2022-08-10 07:26:45 +0300

    homeWork task1 done

---
+++ LibraryService/LibraryService.Web/Models/SearchType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryService.Web.Models
+{
+    public enum SearchTypeEnum
+    {
+        [Display(Name = "Заголовок")]
+        Title,
+
+        [Display(Name = "Автор")]
+        Author,
+
+        [Display(Name = "Категория")]
+        Category,
+    }
+}
