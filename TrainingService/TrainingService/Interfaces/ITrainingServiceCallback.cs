ee696830e33b1bdbe4b1eecfa16eeab38e76122a
Pavel <pavel.official@bk.ru>
2022-08-13 22:20:12 +0300

    Lesson_2. HW done

---
+++ TrainingService/TrainingService/Interfaces/ITrainingServiceCallback.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ServiceModel;
+using TrainingService.ServiceClasses;
+
+namespace TrainingService.Interfaces
+{
+    [ServiceContract]
+    public interface ITrainingServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void UpdateStatistics(StatisticsService statisticsService);
+
+    }
+}
