215870ecc6f578c801a5afae55591bba33d81afe
Pavel <pavel.official@bk.ru>
2022-08-18 07:36:51 +0300

    создал проекты, основные сущности, указал строку подключения к БД

---
+++ ClinicService/ClinicService.Data/Models/Pet.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicService.Data.Models;
+
+[Table("Pets")]
+public class Pet
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int PetId { get; set; }
+
+    [Column]
+    [StringLength(20)]
+    public string Name { get; set; }
+
+    [Column]
+    public DateTime Birthday { get; set; }
+
+
+    [ForeignKey(nameof(Client))]
+    public int ClientId { get; set; }
+
+    public virtual Client Client { get; set; }
+
+
+    [InverseProperty(nameof(Consultation.Pet))]
+    public virtual ICollection<Consultation> Consultations { get; set; } = new HashSet<Consultation>();
+
+}
